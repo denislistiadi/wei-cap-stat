aed1701d4b2e950e682be400b4f15fc022f4b890
Denis Listiadi <denislistiadi24@gmail.com>
2025-07-03 09:33:24 +0700

    feat: update configuration files and add utility functions

--- src/app/chat/page.tsx
+++ src/app/chat/page.tsx
@@ -1,9 +1,7 @@
 export default function ChatPage() {
   return (
     <div className="flex flex-col h-full">
-      <div className="flex-1 overflow-y-auto p-4">
-        {/* Chat messages will go here */}
-      </div>
+      <div className="flex-1 overflow-y-auto p-4">{/* Chat messages will go here */}</div>
       <div className="p-4 border-t">Loh ee</div>
     </div>
   );
