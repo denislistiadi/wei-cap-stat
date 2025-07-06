eaef661acb1e4d383f1dee2bbb1bc930e95b3050
Denis Listiadi <denislistiadi24@gmail.com>
2025-07-06 15:01:57 +0700

    style: standardize string quotes and formatting

--- src/app/api/generate/route.ts
+++ src/app/api/generate/route.ts
@@ -2,7 +2,7 @@ import { type NextRequest, NextResponse } from 'next/server';
 
 import { apiKey, generateEndpoint, modelId } from '@/config/config';
 
-const fetchOpenRouter = async (text: string) => {
+const fetchServer = async (text: string) => {
   if (!apiKey) {
     throw new Error('API key is not set');
   }
@@ -27,16 +27,19 @@ const fetchOpenRouter = async (text: string) => {
     throw new Error(`HTTP error! status: ${resp.status}`);
   }
   const data = await resp.json();
-  console.log('Response from OpenRouter:', data);
   return data;
 };
 
 const extractCaptions = (text: string) => {
   const rawParts = text.split(/\n\d+\.\s/).slice(1);
 
-  return rawParts.map((part) => {
+  return rawParts.map((part, index) => {
     const caption = part.split('*')[0].trim();
-    return caption.replace(/\*\*/g, '').trim();
+    const cleanText = caption.replace(/\*\*/g, '').trim();
+    return {
+      id: index + 1,
+      text: cleanText,
+    };
   });
 };
 
@@ -46,7 +49,7 @@ export async function POST(req: NextRequest) {
   const prompt = `Buatlah 5 caption Instagram yang menarik, elegan, dan unik dalam bahasa Indonesia. Caption-caption ini harus relevan dengan konteks berikut: ${body.text}. Gunakan gaya bahasa yang kreatif, menyentuh emosi, dan sesuai untuk audiens modern di media sosial. Buat dalam bentuk list dengan format 1. Caption pertama, 2. Caption kedua, dst. Pastikan setiap caption tidak lebih dari 100 karakter dan hindari penggunaan tanda baca yang berlebihan. Gunakan gaya bahasa yang sesuai dengan tren saat ini dan mudah dipahami oleh pembaca.`;
 
   try {
-    const resp = await fetchOpenRouter(prompt);
+    const resp = await fetchServer(prompt);
     const data = extractCaptions(resp.choices[0].message.content);
     console.table(resp.choices[0].message.content);
     return NextResponse.json({ data });
