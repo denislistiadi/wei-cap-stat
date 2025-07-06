f200442236139087b80643d9d5a50f77fe5e3402
Denis Listiadi <denislistiadi24@gmail.com>
2025-07-06 14:50:49 +0700

    feat: add configuration file for API keys and endpoints

---
+++ src/config/config.ts
@@ -0,0 +1,3 @@
+export const apiKey = process.env.API_KEY_OPEN_ROUTER || '';
+export const generateEndpoint = process.env.GENERATE_ENDPOINT || '';
+export const modelId = process.env.MODEL_ID || '';
